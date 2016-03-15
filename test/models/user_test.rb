9d2cea2a3ce9c337c10f9813b933bb79702994f8
Pete <peternatewood@gmail.com>
2016-03-15 16:17:21 -0400

    Add user test for missing username

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,4 +9,12 @@ class UserTest < ActiveSupport::TestCase
     })
     assert user.save, "User didn't save despite valid fields!"
   end
+
+  test "Should not save without username" do
+    without_username = User.new({
+      email: "email@example.com",
+      password: "password"
+    })
+    assert_not without_username.save, "User saved without a username!"
+  end
 end
