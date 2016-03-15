4020f874840adf08ec7a1b531d88262875dbe029
Pete <peternatewood@gmail.com>
2016-03-15 16:42:40 -0400

    Add validations to users_product model

--- app/models/users_product.rb
+++ app/models/users_product.rb
@@ -1,2 +1,10 @@
 class UsersProduct < ActiveRecord::Base
+  validates :servings, presence: true, numericality: {only_integer: true}
+  validates :user_id, presence: true, numericality: {only_integer: true}
+  validates :product_id, presence: true, numericality: {only_integer: true}
+  validates :date_eaten, presence: true
+  validates :price, presence: true
+
+  belongs_to :user
+  belongs_to :product
 end
