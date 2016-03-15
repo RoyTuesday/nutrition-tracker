0fa32f5125be926d60f894ad681f3a5e647aef50
Pete <peternatewood@gmail.com>
2016-03-15 16:01:48 -0400

    Add product validation for serving_unit

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   validates :name, presence: true
   validates :category, presence: true
   validates :serving_size, presence: true
+  validates :serving_unit, presence: true
   validates :ndb_no, presence: true, uniqueness: true
 
   has_many :users_products
