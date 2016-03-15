bc479d70f63ef0376f126dd891759b0c389cbba2
Pete <peternatewood@gmail.com>
2016-03-15 15:36:21 -0400

    Add product model

---
+++ app/models/product.rb
@@ -0,0 +1,9 @@
+class Product < ActiveRecord::Base
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :ndb_no, presence: true, uniqueness: true
+
+  has_many :users_products
+  has_many :products_nutrients
+  has_many :nutrients, through: :products_nutrients
+end
