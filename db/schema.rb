0c233a95683588967881a1712b7bea5a0e70b93c
Pete <peternatewood@gmail.com>
2016-03-15 15:55:12 -0400

    Create database and apply migrations

---
+++ db/schema.rb
@@ -0,0 +1,69 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160315191706) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "nutrients", force: :cascade do |t|
+    t.text     "name",            null: false
+    t.text     "category",        null: false
+    t.text     "unit_of_measure", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "nutrients", ["name"], name: "index_nutrients_on_name", using: :btree
+
+  create_table "products", force: :cascade do |t|
+    t.text     "name",         null: false
+    t.text     "category"
+    t.integer  "serving_size", null: false
+    t.string   "serving_unit", null: false
+    t.integer  "ndb_no",       null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "products", ["ndb_no"], name: "index_products_on_ndb_no", using: :btree
+
+  create_table "products_nutrients", force: :cascade do |t|
+    t.integer  "product_id",  null: false
+    t.integer  "nutrient_id", null: false
+    t.integer  "quantity",    null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",        null: false
+    t.string   "email",           null: false
+    t.text     "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+
+  create_table "users_products", force: :cascade do |t|
+    t.integer  "servings",   null: false
+    t.integer  "user_id",    null: false
+    t.integer  "product_id", null: false
+    t.date     "date_eaten", null: false
+    t.string   "price",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
