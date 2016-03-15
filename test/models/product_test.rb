77791aeffc2df455f194524da941bfa153bb469a
Pete <peternatewood@gmail.com>
2016-03-15 15:56:39 -0400

    Add product test for missing name

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -5,4 +5,9 @@ class ProductTest < ActiveSupport::TestCase
     product = Product.new({name: "name", category: "category", serving_size: 1, serving_unit: "g", ndb_no: 123456})
     assert product.save, "Product did not save despite being given valid fields!"
   end
+
+  test "Should not save without name" do
+    without_name = Product.new({category: "category", serving_size: 1, serving_unit: "g", ndb_no: 123456})
+    assert_not without_name.save, "Product saved without name!"
+  end
 end
