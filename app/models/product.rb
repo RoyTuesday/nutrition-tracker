0c367b7a490e46fa0846be898de932930f31cd23
Pete <peternatewood@gmail.com>
2016-03-15 15:59:54 -0400

    Add product validation for serving_size

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   validates :name, presence: true
   validates :category, presence: true
+  validates :serving_size, presence: true
   validates :ndb_no, presence: true, uniqueness: true
 
   has_many :users_products
