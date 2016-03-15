2adc38f502b3b7c9bd24b6b08750a12f4f78b1f9
Pete <peternatewood@gmail.com>
2016-03-15 16:00:30 -0400

    Add product test for missing serving size

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -15,4 +15,9 @@ class ProductTest < ActiveSupport::TestCase
     without_category = Product.new({name: "name", serving_size: 1, serving_unit: "g", ndb_no: 123456})
     assert_not without_category.save, "Product saved without category!"
   end
+
+  test "Should not save without serving size" do
+    without_serving_size = Product.new({name: "name", category: "category", serving_unit: "g", ndb_no: 123456})
+    assert_not without_serving_size.save, "Product saved without serving size!"
+  end
 end
