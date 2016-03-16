bcfdadf7b9a94fe3241bd37ee29c80e50ced1136
Pete <peternatewood@gmail.com>
2016-03-16 15:50:59 -0400

    Add ajax request to handle sessions#create

--- app/assets/javascripts/nav_bar.js
+++ app/assets/javascripts/nav_bar.js
@@ -7,4 +7,30 @@ $(document).ready(function() {
       console.log("sessions/new failure?", response);
     });
   });
+
+  $("#login-container").on("submit", "form", function(event) {
+    event.preventDefault();
+    console.log(event.currentTarget);
+    var form = event.currentTarget;
+    $.ajax({
+      data: $(form).serialize(),
+      dataType: "json",
+      method: form.method,
+      url: form.action
+    }).done(function(response) {
+      console.log("sessions/create success!", response);
+      if(response.errors) {
+        $("#login-errors").empty();
+        response.errors.forEach(function(message) {
+          $("#login-errors").append(message);
+        })
+      }
+      else if(response.userNav) {
+        $("#login-container").empty();
+        $("nav").html(response.userNav);
+      }
+    }).fail(function(response) {
+      console.log("sessions/create failure?", response);
+    });
+  });
 });
