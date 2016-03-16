4e1467b83f174d988962c96d6db1b6489f90b9db
Pete <peternatewood@gmail.com>
2016-03-16 13:18:35 -0400

    Format product test file to be more readable

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,22 +2,43 @@ require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
   test "Should save with valid fields" do
-    product = Product.new({name: "name", category: "category", serving_size: 1, serving_unit: "g", ndb_no: 123456})
+    product = Product.new({
+      name: "name",
+      category: "category",
+      serving_size: 1,
+      serving_unit: "g",
+      ndb_no: 123456
+    })
     assert product.save, "Product did not save despite being given valid fields!"
   end
 
   test "Should not save without name" do
-    without_name = Product.new({category: "category", serving_size: 1, serving_unit: "g", ndb_no: 123456})
+    without_name = Product.new({
+      category: "category",
+      serving_size: 1,
+      serving_unit: "g",
+      ndb_no: 123456
+    })
     assert_not without_name.save, "Product saved without name!"
   end
 
   test "Should not save without category" do
-    without_category = Product.new({name: "name", serving_size: 1, serving_unit: "g", ndb_no: 123456})
+    without_category = Product.new({
+      name: "name",
+      serving_size: 1,
+      serving_unit: "g",
+      ndb_no: 123456
+    })
     assert_not without_category.save, "Product saved without category!"
   end
 
   test "Should not save without serving size" do
-    without_serving_size = Product.new({name: "name", category: "category", serving_unit: "g", ndb_no: 123456})
+    without_serving_size = Product.new({
+      name: "name",
+      category: "category",
+      serving_unit: "g",
+      ndb_no: 123456
+    })
     assert_not without_serving_size.save, "Product saved without serving size!"
   end
 
@@ -55,17 +76,33 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "Should not save without serving unit" do
-    without_serving_unit = Product.new({name: "name", category: "category", serving_size: 1, ndb_no: 123456})
+    without_serving_unit = Product.new({
+      name: "name",
+      category: "category",
+      serving_size: 1,
+      ndb_no: 123456
+    })
     assert_not without_serving_unit.save, "Product saved without serving unit"
   end
 
   test "Should not save without ndb number" do
-    without_ndb_no = Product.new({name: "name", category: "category", serving_size: 1, serving_unit: "g"})
+    without_ndb_no = Product.new({
+      name: "name",
+      category: "category",
+      serving_size: 1,
+      serving_unit: "g"
+    })
     assert_not without_ndb_no.save, "Product saved without ndb number!"
   end
 
   test "Should not save if ndb number already in database" do
-    duplicate_ndb_no = Product.new({name: "name", category: "category", serving_size: 1, serving_unit: "g", ndb_no: products(:beets)})
+    duplicate_ndb_no = Product.new({
+      name: "name",
+      category: "category",
+      serving_size: 1,
+      serving_unit: "g",
+      ndb_no: products(:beets)
+    })
     assert_not duplicate_ndb_no.save, "Product saved desite ndb number already in database!"
   end
 end
