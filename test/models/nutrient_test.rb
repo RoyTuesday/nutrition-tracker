6e72ed65f68d5dee47941aec735241c81295f175
Pete <peternatewood@gmail.com>
2016-03-16 13:24:07 -0400

    Add nutrient test for valid fields

--- test/models/nutrient_test.rb
+++ test/models/nutrient_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class NutrientTest < ActiveSupport::TestCase
+  test "Should save with valid fields" do
+    nutrient = Nutrient.new({
+      name: "name",
+      category: "category",
+      unit_of_measure: "unit"
+    })
+    assert nutrient.save, "Nutrient did not save despite valid fields!"
+  end
 end
