faecfc1bfb3af4c3dfcf741f0a1f5c7a2dd501e6
Pete <peternatewood@gmail.com>
2016-03-16 13:37:25 -0400

    Add greater or equal to 0 validation for products_nutrient quantity

--- app/models/products_nutrient.rb
+++ app/models/products_nutrient.rb
@@ -1,7 +1,7 @@
 class ProductsNutrient < ActiveRecord::Base
   validates :product_id, presence: true, numericality: {only_integer: true}
   validates :nutrient_id, presence: true, numericality: {only_integer: true}
-  validates :quantity, presence: true
+  validates :quantity, presence: true, numericality: {greater_than_or_equal_to: 0}
 
   belongs_to :product
   belongs_to :nutrient
