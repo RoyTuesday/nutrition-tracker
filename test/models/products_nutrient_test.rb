af23fd6356c5ab9ed022a2323955795c90796e0a
Pete <peternatewood@gmail.com>
2016-03-16 13:36:04 -0400

    Add products_nutrient test for valid fields

--- test/models/products_nutrient_test.rb
+++ test/models/products_nutrient_test.rb
@@ -1,4 +1,12 @@
 require 'test_helper'
 
 class ProductsNutrientTest < ActiveSupport::TestCase
+  test "Should save with valid fields" do
+    products_nutrient = ProductsNutrient.new({
+      product: products(:beets),
+      nutrient: nutrients(:water),
+      quantity: 1
+    })
+    assert products_nutrient.save, "Product's nutrient did not save despite valid fields!"
+  end
 end
