b553be399fc5ed7dde3b1b178a9ea4b235cac43c
Pete <peternatewood@gmail.com>
2016-03-16 12:20:28 -0400

    Update users_product model to convert date_eaten string to Date object

--- app/models/users_product.rb
+++ app/models/users_product.rb
@@ -5,6 +5,13 @@ class UsersProduct < ActiveRecord::Base
   validates :date_eaten, presence: true
   validates :price, presence: true
 
+  def initialize args
+    super
+    date_params = args[:date_eaten].split('/')
+    date_params.map!(&:to_i)
+    self.date_eaten = Date.new date_params[2], date_params[0], date_params[1]
+  end
+
   belongs_to :user
   belongs_to :product
 end
