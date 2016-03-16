c177fa8ae26dad6c3e925f90d01631b1c14cf85b
Pete <peternatewood@gmail.com>
2016-03-16 15:20:08 -0400

    Add sessions#create in controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,4 +5,15 @@ class SessionsController < ApplicationController
       render partial: 'form', layout: false, locals: {user: @user}
     end
   end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user and @user.authenticate params[:password]
+      session[:user_id] = @user.id
+      redirect_to :root
+    else
+      @errors = ["Email, password, or both are incorrect"]
+      render partial: "form", layout: false, locals: {user: @user}
+    end
+  end
 end
