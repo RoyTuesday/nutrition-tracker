e369cd1664e50d1e1002fefd9eb7c9745795f934
Pete <peternatewood@gmail.com>
2016-03-16 13:22:08 -0400

    Add validations for nutrient model

--- app/models/nutrient.rb
+++ app/models/nutrient.rb
@@ -1,3 +1,7 @@
 class Nutrient < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :category, presence: true
+  validates :unit_of_measure, presence: true
+
   has_many :products_nutrients
 end
