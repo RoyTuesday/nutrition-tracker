0ce94374df6232e02055ffd85d4e547168f8472c
Pete <peternatewood@gmail.com>
2016-03-16 17:10:00 -0400

    Add user#create in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,25 @@ class UsersController < ApplicationController
       render partial: "form", layout: false, locals: {user: @user}
     end
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      if request.xhr?
+        render json: {userNav: render_to_string("sessions/_user_nav", layout: false, user: @user)}
+      end
+    else
+      @errors = @user.errors.full_messages
+      if request.xhr?
+        render json: {errors: @errors}
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
