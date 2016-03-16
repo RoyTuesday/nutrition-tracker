a8c286186f64a20a1e85a5375f7be418c2af333c
Pete <peternatewood@gmail.com>
2016-03-16 13:33:02 -0400

    Add validations to products_nutrient model

--- app/models/products_nutrient.rb
+++ app/models/products_nutrient.rb
@@ -1,4 +1,8 @@
 class ProductsNutrient < ActiveRecord::Base
+  validates :product_id, presence: true, numericality: {only_integer: true}
+  validates :nutrient_id, presence: true, numericality: {only_integer: true}
+  validates :quantity, presence: true
+
   belongs_to :product
   belongs_to :nutrient
 end
