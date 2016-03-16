e1c9b7f9b97d69ec06db54580a8ad828e9d17910
Pete <peternatewood@gmail.com>
2016-03-16 15:06:12 -0400

    Update sessions controller to render new session form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,8 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+    if request.xhr?
+      render partial: 'form', layout: false, locals: {user: @user}
+    end
+  end
 end
