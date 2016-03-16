35bb582fbb6f843bc78bd6b28a0eb31dd1b8e50f
Pete <peternatewood@gmail.com>
2016-03-16 12:41:39 -0400

    Update migrations to use floats for necessary fields

--- db/migrate/20160315191034_create_products.rb
+++ db/migrate/20160315191034_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.text :name, null: false
       t.text :category
-      t.integer :serving_size, null: false
+      t.float :serving_size, null: false
       t.string :serving_unit, null: false
       t.integer :ndb_no, null: false, unique: true, index: true
 
--- db/migrate/20160315191354_create_users_products.rb
+++ db/migrate/20160315191354_create_users_products.rb
@@ -1,7 +1,7 @@
 class CreateUsersProducts < ActiveRecord::Migration
   def change
     create_table :users_products do |t|
-      t.integer :servings, null: false
+      t.float :servings, null: false
       t.integer :user_id, null: false, foreign_key: true
       t.integer :product_id, null: false, foreign_key: true
       t.date :date_eaten, null: false
