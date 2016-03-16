c3629ab47a9c878a685e0059e4cd520216d8c66e
Pete <peternatewood@gmail.com>
2016-03-16 15:50:27 -0400

    Update sessions#create to return json for xhr requests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,10 +10,18 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:email])
     if @user and @user.authenticate params[:password]
       session[:user_id] = @user.id
-      redirect_to :root
+      if request.xhr?
+        render json: {userNav: render_to_string("_nav", layout: false, user: @user)}
+      else
+        redirect_to :root
+      end
     else
       @errors = ["Email, password, or both are incorrect"]
-      render partial: "form", layout: false, locals: {user: @user}
+      if request.xhr?
+        render json: {errors: @errors}
+      else
+        render partial: "form", layout: false, locals: {user: @user}
+      end
     end
   end
 end
