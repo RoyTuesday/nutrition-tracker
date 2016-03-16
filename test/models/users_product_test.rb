6ebe8245062ab25fea562eeaa38d859b01826319
Pete <peternatewood@gmail.com>
2016-03-16 12:20:49 -0400

    Add users_product test for valid fields

--- test/models/users_product_test.rb
+++ test/models/users_product_test.rb
@@ -1,4 +1,14 @@
 require 'test_helper'
 
 class UsersProductTest <ActiveSupport::TestCase
+  test "Should save with valid fields" do
+    users_product = UsersProduct.new({
+      servings: 1,
+      user: users(:charles),
+      product: products(:beets),
+      date_eaten: "3/16/2016",
+      price: "$1.00"
+    })
+    assert users_product.save, "User's product didn't save despite valid fields!"
+  end
 end
