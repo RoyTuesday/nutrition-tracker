fd176e19f31a0809e4ff6407fb4b9e28ea75f6b0
Pete <peternatewood@gmail.com>
2016-03-17 15:00:37 -0400

    Add products#ndb_search in controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,10 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def ndb_search
+    if request.xhr?
+      render json: {status: 200}
+    end
+  end
 end
