bbb5879964f42ffaf09a953dba5bddff7036ac55
Pete <peternatewood@gmail.com>
2016-03-17 15:36:27 -0400

    Update products#ndb_search to get usda food search and return response as json

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,15 @@
 class ProductsController < ApplicationController
+  SEARCH_URI = "http://api.nal.usda.gov/ndb/search"
+  API_KEY = ENV["USDA_FOOD_API_KEY"]
+
   def index
     @products = Product.all
   end
 
   def ndb_search
     if request.xhr?
-      render json: {status: 200}
+      response = HTTP.get("#{SEARCH_URI}/?format=json&q=butter&api_key=#{API_KEY}")
+      render json: {status: 200, response: response.parse}
     end
   end
 end
