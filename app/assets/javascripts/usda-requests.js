961962b911525d7aadfc15a6f4d24a2639e64ce0
Pete <peternatewood@gmail.com>
2016-03-17 18:35:23 -0400

    Add ajax for new product form

--- app/assets/javascripts/usda-requests.js
+++ app/assets/javascripts/usda-requests.js
@@ -17,16 +17,36 @@ $(document).ready(function() {
 
   $("#ndb-search-results").on("click", "a.food-item", function(event) {
     event.preventDefault();
+    var productData = {
+      name: this.dataset["name"],
+      category: this.dataset["category"],
+      ndb_no: this.dataset["ndbNo"]
+    }
     var link = this;
     $.ajax({
-      data: {ndb_no: link.dataset.ndbNumber},
+      data: productData,
       dataType: "json",
       method: "GET",
       url: link.href
     }).done(function(response) {
-      console.log("Search result link success!", response);
+      $('main').html(response.html);
     }).fail(function(response) {
       console.log("Search result link failure?", response);
+    });
+  });
+
+  $("main").on("submit", "#new_product", function(event) {
+    event.preventDefault();
+    var form = this;
+    $.ajax({
+      data: $(form).serialize(),
+      dataType: 'json',
+      method: form.method,
+      url: form.action
+    }).done(function(response) {
+      $("main").html(response.html)
+    }).fail(function(response) {
+      console.log("New product failure?", response);
     });
   });
 });
