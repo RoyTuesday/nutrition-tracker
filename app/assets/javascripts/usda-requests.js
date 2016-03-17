2505de2d38a275a21ba4067c73e18830620b9bd3
Pete <peternatewood@gmail.com>
2016-03-17 15:02:19 -0400

    Add ajax request for ndb-search form

---
+++ app/assets/javascripts/usda-requests.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#usda-ndb-search").on("submit", function(event) {
+    event.preventDefault();
+    var form = this;
+    $.ajax({
+      data: $(form).serialize(),
+      dataType: 'json',
+      method: form.method,
+      url: form.action
+    }).done(function(response) {
+      console.log("NDB search success!", response);
+    }).fail(function(response) {
+      console.log("NDB search success!", response);
+    });
+  });
+});
