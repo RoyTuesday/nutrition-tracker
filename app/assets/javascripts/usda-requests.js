1f024739b14677b86953255b109717a152f3a351
Pete <peternatewood@gmail.com>
2016-03-17 17:08:21 -0400

    Add ajax call for food-item links

--- app/assets/javascripts/usda-requests.js
+++ app/assets/javascripts/usda-requests.js
@@ -14,4 +14,19 @@ $(document).ready(function() {
       console.log("NDB search success!", response);
     });
   });
+
+  $("#ndb-search-results").on("click", "a.food-item", function(event) {
+    event.preventDefault();
+    var link = this;
+    $.ajax({
+      data: {ndb_no: link.dataset.ndbNumber},
+      dataType: "json",
+      method: "GET",
+      url: link.href
+    }).done(function(response) {
+      console.log("Search result link success!", response);
+    }).fail(function(response) {
+      console.log("Search result link failure?", response);
+    });
+  });
 });
