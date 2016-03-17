96721f6388651ce9fa778af27e5358ba68bb8ae8
Pete <peternatewood@gmail.com>
2016-03-17 18:35:04 -0400

    Update product#create to create product and associated records

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -26,6 +26,38 @@ class ProductsController < ApplicationController
   end
 
   def create
+    if request.xhr?
+      product = Product.new(product_params)
+      if product.save
+        response = HTTP.get("#{REPORT_URI}/?format=json", params: {
+          ndbno: product_params[:ndb_no],
+          api_key: API_KEY
+        })
+        nutrients = response.parse["report"]["food"]["nutrients"]
+        nutrients.each do |nutrient|
+          current_nutrient = Nutrient.find_by(name: nutrient["name"])
+          unless current_nutrient
+            current_nutrient = Nutrient.create({
+              name: nutrient["name"],
+              category: nutrient["group"],
+              unit_of_measure: nutrient["unit"]
+            })
+          end
+          ProductsNutrient.create({
+            product: product,
+            nutrient: current_nutrient,
+            quantity: nutrient["value"]
+          })
+        end
+        @products = Product.all
+        render json: {html: render_to_string(
+          "index",
+          layout: false,
+        )}
+      else
+        render json: {response: product.errors.full_messages}, status: 500
+      end
+    end
   end
 
   def ndb_search
@@ -43,5 +75,11 @@ class ProductsController < ApplicationController
         )}
       end
     end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :category, :serving_size, :serving_unit, :ndb_no)
   end
 end
