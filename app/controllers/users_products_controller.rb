6468301d48ed6c937b021cf537ed224897f89653
Pete <peternatewood@gmail.com>
2016-03-18 11:08:31 -0400

    Add users_product#new in controller

--- app/controllers/users_products_controller.rb
+++ app/controllers/users_products_controller.rb
@@ -1,2 +1,18 @@
 class UsersProductsController < ApplicationController
+  def new
+    product = Product.find_by(id: params[:product_id])
+    users_product = UsersProduct.new
+    if request.xhr?
+      if product
+        render json: {
+          form: render_to_string("_form", layout: false, locals: {
+            product: product,
+            users_product: users_product
+          })
+        }
+      else
+        render json: {product: product}, status: 500
+      end
+    end
+  end
 end
