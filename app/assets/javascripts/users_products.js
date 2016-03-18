89e0762a57b4bfa7e93f4a2e500d3d3cb6452535
Pete <peternatewood@gmail.com>
2016-03-18 11:38:30 -0400

    Update users_products new ajax to toggle form if already loaded

--- app/assets/javascripts/users_products.js
+++ app/assets/javascripts/users_products.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
   $("#products-list").on("click", "a.product", function(event) {
     event.preventDefault();
-    $.ajax({
-      dataType: "json",
-      url: this.href
-    }).done(function(response) {
-      console.log("Users product form success!", response);
-    }).fail(function(response) {
-      console.log("Users product form failure?", response);
-    });
+    
+    var productId = event.currentTarget.dataset["id"];
+    var productForm = $("#product-" + productId + " form");
+
+    if(productForm.length > 0) {
+      productForm.toggle();
+    }
+    else {
+      $.ajax({
+        dataType: "json",
+        url: this.href
+      }).done(function(response) {
+        $("#product-" + response.product.id).append(response.form);
+      }).fail(function(response) {
+        console.log("Users product form failure?", response);
+      });
+    }
   });
 });
