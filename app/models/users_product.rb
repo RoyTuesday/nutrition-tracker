4966c2b70881d3e45c8fa7ac1dd25ba9ddf61f2f
Pete <peternatewood@gmail.com>
2016-03-30 14:47:15 -0400

    Remove date formatting from users_product model

--- app/models/users_product.rb
+++ app/models/users_product.rb
@@ -5,24 +5,10 @@ class UsersProduct < ActiveRecord::Base
   validates :date_eaten, presence: true
   validates :price, presence: true
 
-  def initialize args = {}
-    super
-    if /\d{1,}\/\d{1,}\/\d{4}/.match args[:date_eaten]
-      self.date_eaten = format_date args[:date_eaten]
-    end
-  end
-
   belongs_to :user
   belongs_to :product
 
   delegate :name, to: :product
   delegate :category, to: :product
   delegate :serving_size, to: :product
-
-  private
-
-  def format_date date
-    date_params = date.split('/').map!(&:to_i)
-    Date.new date_params[2], date_params[0], date_params[1]
-  end
 end
