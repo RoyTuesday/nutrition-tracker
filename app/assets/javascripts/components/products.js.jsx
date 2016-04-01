07c2547c0da2aee9fb7cc62fca5cdcce605e43cf
Pete <peternatewood@gmail.com>
2016-04-01 16:33:47 -0400

    Update ProductList to render simple message if no products found

--- app/assets/javascripts/components/products.js.jsx
+++ app/assets/javascripts/components/products.js.jsx
@@ -1,35 +1,72 @@
 var Product = React.createClass({
+  getInitialState: function() {
+    return {detailsShown: false};
+  },
+
+  handleClick: function(event) {
+    event.preventDefault();
+    this.setState({detailsShown: !this.state.detailsShown});
+  },
+
+  hideDetails: function() {
+    this.setState({detailsShown: false})
+  },
+
   render: function() {
+    var usersProductForm;
+    if(this.props.isLoggedIn) {
+      usersProductForm = (
+        <UsersProductForm authenticityToken={this.props.authenticityToken} hideDetails={this.hideDetails} url={"/products/" + this.props.product.id + "/users_products"} usersProduct={{servings: "", date_eaten: "", price: ""}} />
+      );
+    }
+
+    var details = "";
+    if(this.state.detailsShown) {
+      details = (
+        <div>
+          <div className="product-details" id={"product-details-" + this.props.product.id}>
+            <p>var ProductSearch = React.createClass({
+
+              Category: {this.props.product.category}
+            </p>
+            <p>
+              Serving size: {this.props.product.serving_size + this.props.product.serving_unit}
+            </p>
+          </div>
+          {usersProductForm}
+        </div>
+      );
+    }
+
     return (
       <div id={"product-" + this.props.product.id}>
         <h3 key={this.props.key}>
-          <a className="product" data-id={this.props.product.id} href={"products/" + this.props.product.id + "/users_products/new"}>
+          <a className="product" href="#" onClick={this.handleClick}>
             {this.props.product.name}
           </a>
         </h3>
-        <div className="product-details" id={"product-details-" + this.props.product.id}>
-          <p>
-            Category: {this.props.product.category}
-          </p>
-          <p>
-            Serving size: {this.props.product.serving_size + this.props.product.serving_unit}
-          </p>
-        </div>
-        <div id={"product-" + this.props.product.id + "-form-container"}>
-        </div>
+        {details}
       </div>
     );
   }
 });
 var ProductList = React.createClass({
   render: function() {
-    var productNodes = this.props.products.map(function(product, index) {
-      return (
-        <li key={"product" + index}>
-          <Product product={product} />
-        </li>
-      );
-    });
+    var productNodes = (
+      <li>
+        No products found!
+      </li>
+    );
+
+    if(this.props.products.length > 0) {
+      productNodes = this.props.products.map(function(product, index) {
+        return (
+          <li key={"product" + index}>
+            <Product authenticityToken={this.props.authenticityToken} isLoggedIn={this.props.isLoggedIn} product={product} />
+          </li>
+        );
+      }.bind(this));
+    }
 
     return (
       <ul>
