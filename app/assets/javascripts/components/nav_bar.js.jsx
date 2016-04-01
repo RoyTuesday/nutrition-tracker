679142a018b5d8a2a98b55cb57e4d81d94285b32
Pete <peternatewood@gmail.com>
2016-04-01 17:20:51 -0400

    Update NavBar component to render session links conditionally

--- app/assets/javascripts/components/nav_bar.js.jsx
+++ app/assets/javascripts/components/nav_bar.js.jsx
@@ -1,7 +1,33 @@
 var NavBar = React.createClass({
   render: function() {
+    var sessionLinks = (
+      <a href="#">
+        Login
+      </a> | 
+      <a href="#">
+        Register
+      </a>
+    );
+    if(this.props.isLoggedIn) {
+      sessionLinks = (
+        <a href="#">
+          {this.props.user.name}
+        </a> | 
+        <a href="#">
+          Log out
+        </a>
+      );
+    }
+
     return (
-      <div></div>
+      <nav>
+        <p>
+          <a href="/">
+            Home
+          </a> | 
+          {sessionLinks}
+        </p>
+      </nav>
     );
   }
 });
