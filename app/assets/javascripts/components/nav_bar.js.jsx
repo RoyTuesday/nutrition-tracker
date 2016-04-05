bf4cb189c7ce6120051ecf80a52898ba05a65ab2
Pete <peternatewood@gmail.com>
2016-04-05 15:24:01 -0400

    Add component for login/register form

--- app/assets/javascripts/components/nav_bar.js.jsx
+++ app/assets/javascripts/components/nav_bar.js.jsx
@@ -1,3 +1,32 @@
+var LoginRegisterForm = React.createClass({
+  render: function() {
+    var usernameField;
+    if(this.props.isRegisterForm) {
+      usernameField = (
+        <label htmlFor="username">Username</label>
+        <input id="username" placeholder="my_name" type="text" value={this.state.username}/>
+      );
+    }
+
+    return (
+      <form>
+        <input name="authenticity_token" type="hidden" value={this.props.authenticityToken}/>
+        <fieldset>
+          {usernameField}
+
+          <label htmlFor="email">Email</label>
+          <input id="email" placeholder="email@example.com" type="email" value={this.state.email}/>
+
+          <label htmlFor="password">Password</label>
+          <input id="password" placeholder="password123" type="password" value={this.state.password}/>
+
+          <input type="submit" value="Log in"/>
+        </fieldset>
+      </form>
+    );
+  }
+});
+
 var NavBar = React.createClass({
   render: function() {
     var sessionLinks = (
@@ -11,7 +40,7 @@ var NavBar = React.createClass({
     if(this.props.isLoggedIn) {
       sessionLinks = (
         <a href="#">
-          {this.props.user.name}
+          {this.props.currentUser.username}
         </a> | 
         <a href="#">
           Log out
