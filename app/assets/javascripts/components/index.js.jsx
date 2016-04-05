7545b56034e05d1fa15a89f8fb743de2758ff5d6
Pete <peternatewood@gmail.com>
2016-04-05 16:11:32 -0400

    Update Index component to render NavBar

--- app/assets/javascripts/components/index.js.jsx
+++ app/assets/javascripts/components/index.js.jsx
@@ -2,15 +2,19 @@ var Index = React.createClass({
   getInitialState: function() {
     return {
       currentUser: this.props.currentUser,
-      isLoggedIn: this.props.isLoggedIn
+      isLoggedIn: !!this.props.currentUser
     }
   },
 
   render: function() {
     return (
-      <main>
-        <ProductsIndex authenticityToken={this.props.authenticityToken} isLoggedIn={this.state.isLoggedIn} products={this.props.products} urls={this.props.urls} />
-      </main>
+      <div>
+        <NavBar authenticityToken={this.props.authenticityToken} currentUser={this.state.currentUser} isLoggedIn={this.state.isLoggedIn} urls={this.props.urls} />
+
+        <main>
+          <ProductsIndex authenticityToken={this.props.authenticityToken} isLoggedIn={this.state.isLoggedIn} products={this.props.products} urls={this.props.urls} />
+        </main>
+      </div>
     );
   }
 });
