6ae156018cc8c0fda3ba048168fde0c630395d3e
Pete <peternatewood@gmail.com>
2016-04-06 14:21:08 -0400

    Update Index to assign authenticityToken prop to UserShow

--- app/assets/javascripts/components/index.js.jsx
+++ app/assets/javascripts/components/index.js.jsx
@@ -31,7 +31,7 @@ var Index = React.createClass({
     switch(this.state.currentPage) {
       case "products": page = <ProductsIndex authenticityToken={this.props.authenticityToken} isLoggedIn={this.state.isLoggedIn} products={this.props.products} urls={this.props.urls} />;
       break;
-      case "user": page = <UserShow currentUser={this.state.currentUser} />;
+      case "user": page = <UserShow authenticityToken={this.props.authenticityToken} currentUser={this.state.currentUser} />;
       break;
       default: page = <ProductsIndex authenticityToken={this.props.authenticityToken} isLoggedIn={this.state.isLoggedIn} products={this.props.products} urls={this.props.urls} />;
       break;
