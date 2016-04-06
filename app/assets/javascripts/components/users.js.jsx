ecd36ec56d6f8f8dcb5f84d6981245b645033957
Pete <peternatewood@gmail.com>
2016-04-06 13:56:22 -0400

    Update UserShow to get usersProducts from server on mounting

--- app/assets/javascripts/components/users.js.jsx
+++ app/assets/javascripts/components/users.js.jsx
@@ -1,10 +1,29 @@
 var UserShow = React.createClass({
+  componentDidMount: function() {
+    $.ajax({
+      dataType: "json",
+      method: "GET",
+      url: "/users_products",
+      success: function(data) {
+        this.setState({usersProducts: data});
+      }.bind(this),
+      fail: function(xhr, status, err) {
+        console.log("Users products index failure?", status, err);
+      }
+    });
+  },
+
+  getInitialState: function() {
+    return {usersProducts: new Array}
+  },
+
   render: function() {
     return(
       <div>
         <h2>
           Welcome, {this.props.currentUser.username}!
         </h2>
+        <UsersProductList usersProducts={this.state.usersProducts} />
       </div>
     );
   }
