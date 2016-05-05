ddae12c8804a401d1fa99ce432d54862379ab115
Pete <peternatewood@gmail.com>
2016-05-05 11:38:55 -0400

    Add #date_range in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def nutrients_totals
     user = User.includes(:users_products).find_by id: session[:user_id]
     if request.xhr?
-      render json: user.get_nutrients_totals(params[:date_range]).as_json
+      render json: user.get_nutrients_totals(date_range).as_json
     end
   end
 
@@ -34,5 +34,19 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def date_range
+    if params[:start_date].length > 0
+      start_date = params[:start_date].to_date
+      if params[:end_date].length > 0
+        end_date = params[:end_date].to_date
+        return start_date..end_date
+      else
+        return start_date..start_date
+      end
+    else
+      return false
+    end
   end
 end
