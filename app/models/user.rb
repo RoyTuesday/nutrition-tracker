0cf8fa577e6f6787326e6d75d9bc055d7384441c
Pete <peternatewood@gmail.com>
2016-05-09 13:40:23 -0400

    Add User method to get nutrients over date range

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,25 @@ class User < ActiveRecord::Base
 
     return totals
   end
+
+  def get_nutrients_over_time nutrient_name, date_range = nil
+    users_products = self.users_products.to_a
+    if date_range
+      users_products.select! do |users_product|
+        date_range.include? users_product.date_eaten
+      end
+    end
+
+    nutrients = Hash.new(0)
+
+    users_products.each do |users_product|
+      users_product.products_nutrients.each do |products_nutrient|
+        if products_nutrient.name == nutrient_name
+          nutrients[users_product.date_eaten] += products_nutrient.quantity
+        end
+      end
+    end
+
+    return nutrients
+  end
 end
