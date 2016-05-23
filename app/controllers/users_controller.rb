dedeaa1b4000c361157a507207aabc199dfbd296
Pete <peternatewood@gmail.com>
2016-05-23 16:27:07 -0400

    Remove unused UsersController.date_range method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,18 +28,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-
-  def date_range
-    if params[:start_date].length > 0
-      start_date = params[:start_date].to_date
-      if params[:end_date].length > 0
-        end_date = params[:end_date].to_date
-        return start_date..end_date
-      else
-        return start_date..start_date
-      end
-    else
-      return Date.new(0)..Date.today
-    end
-  end
 end
