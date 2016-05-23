a0c8d7b3f481642222a1e1582bbae93b323a61fd
Pete <peternatewood@gmail.com>
2016-05-23 15:41:16 -0400

    Add method in Nutrients controller to get date range

--- app/controllers/nutrients_controller.rb
+++ app/controllers/nutrients_controller.rb
@@ -1,4 +1,20 @@
 class NutrientsController < ApplicationController
   def totals
   end
+
+  private
+
+  def date_range
+    if params[:start_date].length > 0
+      start_date = params[:start_date].to_date
+      if params[:end_date].length > 0
+        end_date = params[:end_date].to_date
+        return start_date..end_date
+      else
+        return start_date..start_date
+      end
+    else
+      return Date.new(0)..Date.today
+    end
+  end
 end
